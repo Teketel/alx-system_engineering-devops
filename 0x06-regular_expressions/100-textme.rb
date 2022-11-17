687c265184a5d78a3351decda4852263927616e8
teketel <for.teketel@gmail.com>
2022-11-17 19:36:11 +0300

    Done Bonus ask of 0x06-regular_expressions

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,6 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hbt+n/).join
+require 'set'
+
+result = ARGV[0].scan(/(from:((?<from>\w+)|(?<from>\+1\d{10,10}))).+(\[to:((?<to>\w+)|(?<to>\+1\d{10,10}))).+(\[flags:(?<flags>\S+)\])/)
+filtered = result[0].reject{|item| item.nil? || item == '' || item.to_s.empty?}
+puts filtered.join(',')
